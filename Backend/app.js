f6e10963ec406c3f4998a8e04f4c69ce75074e5d
Ashish Verma <akumarverma654@gmail.com>
2026-04-06 10:14:57 +0530

    added the frontend url in cors

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ const app = express();
 // Security and Parsing Middlewares
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://kaali-kahani-tpkx.vercel.app',
   credentials: true
 }));
 app.use(express.json());
